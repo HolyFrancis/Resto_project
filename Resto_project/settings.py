9b8480a16265d7be13a00de2d1a8f02ccb2d9cb6
dholy <holyfrancisdushime@gmail.com>
2023-06-02 00:29:30 +0200

    database configuration in settings

--- Resto_project/settings.py
+++ Resto_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Resto_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'resto_db',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306'
     }
 }
 
