3f74f99d948c26dc77dff0b6d762714ab62af312
dholy <holyfrancisdushime@gmail.com>
2023-06-04 21:36:06 +0200

    loginUser creation

--- app/views/account.py
+++ app/views/account.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from app.forms import UserRegisterForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 
 def registerPage(request):
@@ -13,4 +15,18 @@ def registerPage(request):
            form.save()
     context={'form':form}
     return render(request, 'app/account/register.html',context)
-    
+
+def loginUser(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        
+        user=authenticate(request,username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'username or password is incorrect')
+    context={}
+    return render(request, 'app/account/login.html',context)
