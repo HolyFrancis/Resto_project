61a529452ddc6f896309c1a43523c4fcffe178a7
dholy <holyfrancisdushime@gmail.com>
2023-06-04 21:40:33 +0200

    add login path

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from app.views import home, account
 urlpatterns = [
     path('',home.index,name='home'),
     path('register',account.registerPage,name='register'),
+    path('login',account.loginUser,name='login'),
 ]
