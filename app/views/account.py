86bf0770607ea07ee543500120e8cf305de605fd
dholy <holyfrancisdushime@gmail.com>
2023-06-04 20:45:36 +0200

    use the UserRegisterForm to register user

--- app/views/account.py
+++ app/views/account.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from app.forms import UserRegisterForm
 
 
 def registerPage(request):
-    form= UserCreationForm()
+    form= UserRegisterForm()
     
     if request.method=='POST':
-       form= UserCreationForm(request.POST)
+       form= UserRegisterForm(request.POST)
        if form.is_valid():
            form.save() 
     context={'form':form}
